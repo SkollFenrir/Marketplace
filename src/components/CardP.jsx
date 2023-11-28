ef4c98261c8f5d906ccbb3d7d9cdae7f7b011790
Diego H <diegoadonis2@gmail.com>
2023-11-28 20:03:20 -0300

    Primer commit en una nueva rama

---
+++ src/components/CardP.jsx
@@ -0,0 +1,7 @@
+import React, { useContext } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+const CardP= ()=>{}
+
+export default CardP;
