3ad578bdb91c30e416e92423304cc59e36379d77
Diego H <diegoadonis2@gmail.com>
2023-12-02 14:24:59 -0300

    Se actualiza el componete CardP

--- src/components/CardP.jsx
+++ src/components/CardP.jsx
@@ -1,21 +1,28 @@
-import React, { useContext } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import React from 'react';
+import { Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
-const CardP = () => {
+const CardP = ({ product }) => {
+	/* const navigate = useNavigate();
+	const navigateProduct = (id) => {  
+		navigate(`/Product/${id}`); 
+	}; */     //Pendiente de uso 
+	const { id, img, title, desc, price, status } = product;
 	return (
-		<Card style={{ width: '18rem' }}>
+		<Card
+			/* onClick={() => navigateProduct(id)} */ //Pendiente de uso
+			style={{ width: '18rem' }}>
 			<Card.Img
 				variant='top'
-				src='holder.js/100px180'
+				src={img}
 			/>
 			<Card.Body>
-				<Card.Title>Card Title</Card.Title>
-				<Card.Text>
-					Some quick example text to build on the card title and make up the
-					bulk of the card's content.
-				</Card.Text>
-				<Button variant='primary'>Go somewhere</Button>
+				<Card.Title>{title}</Card.Title>
+				<Card.Text>{desc}</Card.Text>
+				<Card.Body>
+					<Card.Text>{price}</Card.Text>
+					<Card.Text>{status}</Card.Text>
+				</Card.Body>
 			</Card.Body>
 		</Card>
 	);
