3bd383f8f320a24fe9441756079c0ed27eeaefd7
avasquezdm <a.vasquezdm@gmail.com>
2023-12-14 21:39:50 -0300

    corazón de melón, digo, de favoritos

--- src/components/CardP.jsx
+++ src/components/CardP.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import Heart from './Heart';
 
 const CardP = ({ product }) => {
-	const navigate = useNavigate();
-	const navigateProduct = (id) => {  
-		navigate(`/Product/${id}`); 
-	};      //Pendiente de uso 
-	const { id, img, title, desc, price, status } = product;
-	return (
-		<Card className='card'
-			onClick={() => navigateProduct(id)}  //Pendiente de uso
-			style={{ width: '18rem' }}>
-			<Card.Img
-				variant='top'
-				src={img}
-			/>
-			<Card.Body>
-				<Card.Title>{title}</Card.Title>
-				<Card.Text>{desc}</Card.Text>
-				<Card.Body>
+    const navigate = useNavigate();
+    const navigateProduct = (id) => {
+        navigate(`/Product/${id}`);
+    };
+    const { id, img, title, desc, price, status } = product;
+
+    return (
+        <Card className='card'>
+            <Card.Img variant='top' src={img} />
+            
+            <div className='like-btn'>
+                <Heart 
+					// onClick={() => setFavoritos(foto.id)}	--> con un clic, se agrega o desagrega a favoritos
+					// filled={foto.favorito}					--> el estado maneja si está o no
+					></Heart>
+            </div>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{desc}</Card.Text>
+                <Card.Body>
+                    <Button className='primary-btn' onClick={() => navigateProduct(id)} >Ver detalle</Button>
 					<Card.Text>{price}</Card.Text>
-					<Card.Text>{status}</Card.Text>
-				</Card.Body>
-			</Card.Body>
-		</Card>
-	);
+                    <Card.Text>{status}</Card.Text>
+                </Card.Body>
+            </Card.Body>
+        </Card>
+    );
 };
 
 export default CardP;
