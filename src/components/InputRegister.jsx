57d0f1dc7117c67ca5b3ffeda882ac94ca04589d
Diego H <diegoadonis2@gmail.com>
2023-12-18 22:36:38 -0300

    register operativo

--- src/components/InputRegister.jsx
+++ src/components/InputRegister.jsx
@@ -1,7 +1,31 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Button, Form, InputGroup } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 export default function InputRegister() {
 
+  const navigate = useNavigate();
+  const [usuario, setUsuario] = useState({});
+
+  const handleSetUsuario = ({ target: { value, name } }) => {
+    const field = {};
+    field[name] = value;
+    setUsuario({ ...usuario, ...field });
+  };
+
+  const registrarUsuario = async () => {
+    const urlServer = "http://localhost:3000";
+    const endpoint = "/register";
+    try {
+      await axios.post(urlServer + endpoint, usuario);
+      alert("Usuario registrado con éxito");
+      navigate("/login");
+    } catch (error) {
+      alert("Algo salió mal ...");
+      console.log(error);
+    }
+  };
 
     return (
       < >
@@ -9,50 +33,63 @@ export default function InputRegister() {
       <InputGroup className='mb-3 mt-5'>
 				<InputGroup.Text id='basic-addon1'>Nombre</InputGroup.Text>
 				<Form.Control
-					//onChange={} todavía no se usará
 					placeholder='Juan'
 					aria-label='name'
+          name="nombre"
+          value={usuario.nombre}
+          onChange={handleSetUsuario}
 				/>
       </InputGroup>
           
       <InputGroup className='mb-3'>
 				<InputGroup.Text id='basic-addon1'>Apellido</InputGroup.Text>
 				<Form.Control
-					//onChange={} todavía no se usará
 					placeholder='Pérez'
 					aria-label='last-name'
+          name="apellido"
+          value={usuario.apellido}
+          onChange={handleSetUsuario}
 				/>
       </InputGroup>
           
       <InputGroup className='mb-3'>
 				<InputGroup.Text id='basic-addon1'>Correo</InputGroup.Text>
 				<Form.Control
-					//onChange={} todavía no se usará
 					placeholder='juan.perez@gmail.com'
 					aria-label='email'
           aria-aria-describedby='correo electrónico del usuario'
+          name="correo"
+          value={usuario.correo}
+          onChange={handleSetUsuario}
 				/>
       </InputGroup>  
         
       <InputGroup className='mb-3'>
 				<InputGroup.Text id='basic-addon1'>Contraseña</InputGroup.Text>
 				<Form.Control
-					//onChange={} todavía no se usará
 					placeholder='***************'
 					aria-label='password'
           aria-aria-describedby='contraseña del usuario'
           type="password"
+          name="contrasena"
+          value={usuario.contrasena}
+          onChange={handleSetUsuario}
 				/>
       </InputGroup>
   
-      <Form.Select aria-label="Default select example">
+      <Form.Select 
+        aria-label="Default select example"
+        name="genero"
+        value={usuario.genero}
+        onChange={handleSetUsuario}
+        >
         <option>Selecciona tu género</option>
         <option value="femenine">Femenino</option>
         <option value="masculine">Masculino</option>
         <option value="other">Otro</option>
       </Form.Select>
 
-        <Button className='mt-3 primary-btn' /* onClick={} */>
+        <Button className='mt-3 primary-btn' onClick={registrarUsuario}>
           Enviar formulario
         </Button>
       </>
