e929d03e9338b8c3a0715ed0a8625f3a0d76a226
Diego H <diegoadonis2@gmail.com>
2023-12-18 21:18:38 -0300

    Login3, ahora sí es personal

--- src/components/InputLogin.jsx
+++ src/components/InputLogin.jsx
@@ -2,31 +2,38 @@ import React, { useContext, useState } from 'react';
 import AuthContext from '../Contexts/AuthContext.js';
 import { useNavigate } from 'react-router-dom';
 import { Button, Form, InputGroup } from 'react-bootstrap';
-import { postLogin } from '../axios/axios.js';
+import axios from 'axios';
 
 export default function InputLogin() {
-	const { usuario, setUsuario } = useContext(AuthContext);
+	const { setUsuario } = useContext(AuthContext);
 	const navigate = useNavigate();
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const [usuario, setUsuarioLocal] = useState({});
 
-	const handleSubmit = async (e) => {
+	const handleSetUsuario = ({ target: { value, name } }) => {
+		const field = {};
+		field[name] = value;
+		setUsuarioLocal({ ...usuario, ...field });
+	};
+
+	const handleLogin = async (e) => {
 		e.preventDefault();
+    const url = 'http://localhost:3000';
+    const endPoint = '/login';
 		try {
-			const user = await postLogin({ correo : email, contrasena :  password });
-      console.log(user)
-      setUsuario(user)
-      setEmail('')
-      setPassword('')
-			return navigate('/profile');
+			const { data: token } = await axios.post(url + endPoint, usuario);
+			alert('Usuario identificado con éxito 😀');
+			window.localStorage.setItem('token', token);
+			console.log( usuario );
+			setUsuario( token);
+			navigate('/profile');
 		} catch (error) {
 			alert('Hubo un error con los datos entregados. Vuelve a intentarlo.');
 		}
 	};
-  console.log(email + password)
+
 	const handleKeyPress = (e) => {
 		if (e.key === 'Enter') {
-			handleSubmit(e);
+			handleLogin(e);
 		}
 	};
 
@@ -35,9 +42,10 @@ export default function InputLogin() {
 			<InputGroup className='mb-3 mt-5'>
 				<InputGroup.Text id='basic-addon1'>Correo</InputGroup.Text>
 				<Form.Control
-					onChange={({target}) => setEmail(target.value)}
+					onChange={handleSetUsuario}
 					placeholder='juan.perez@gmail.com'
-          value={email}
+					value={usuario.correo}
+					name='correo'
 					aria-label='email'
 					onKeyDown={handleKeyPress}
 				/>
@@ -46,8 +54,9 @@ export default function InputLogin() {
 			<InputGroup className='mb-3'>
 				<InputGroup.Text id='basic-addon1'>Contraseña</InputGroup.Text>
 				<Form.Control
-					onChange={({target}) => setPassword(target.value)}
-          value ={password}
+					onChange={handleSetUsuario}
+					value={usuario.contrasena}
+					name='contrasena'
 					placeholder='***************'
 					aria-label='password'
 					type='password'
@@ -57,7 +66,7 @@ export default function InputLogin() {
 
 			<Button
 				className='mt-3 primary-btn'
-				onClick={handleSubmit}>
+				onClick={handleLogin}>
 				Iniciar Sesión
 			</Button>
 		</>
